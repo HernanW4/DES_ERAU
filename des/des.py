741f34b4f4963c91ce33940b9833948f94f73469
CallalilyR <52506764+CallalilyR@users.noreply.github.com>
2023-05-01 19:39:30 -0400

    perm function tested through encrypt funtion

--- des/des.py
+++ des/des.py
@@ -1,6 +1,7 @@
 # The actual algorithm, encryption, decryption and what not.
-from .utils import hex_to_bin
+from .utils import *
 from .key import Key
+
 
 def bit_xor(a,b):
     result = ""
@@ -10,3 +11,25 @@ def bit_xor(a,b):
         else:
             result += "1"
     return result
+
+#rearrangement of plaintext bits according to a given permutation table.
+def applyInitialPerm(pTable, plainText):
+    perm_plaintext = ""
+
+    for index in pTable:
+        perm_plaintext += plainText[int(index)-1]
+    return perm_plaintext
+
+def encrypt():
+     userInput = '0123456789ABCDEF'
+
+     plaintext = hex_to_bin(userInput)
+     print(plaintext)
+
+
+     perm_plaintext = applyInitialPerm(initial_perm_table, plaintext)
+     
+     print(perm_plaintext)
+
+
+encrypt()
