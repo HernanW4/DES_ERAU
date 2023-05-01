9139d3cf0cb4f5364839878e89f0e0b88675ddb3
meholico <meholico@my.erau.edu>
2023-05-01 19:07:37 -0400

    Added XOR function

--- des/des.py
+++ des/des.py
@@ -2,10 +2,11 @@
 from .utils import hex_to_bin
 from .key import Key
 
-def greeting():
-    print("Hello World")
-
-
-def testing_hex_to_bin():
-    value = "F"
-    print(hex_to_bin(value))
+def bit_xor(a,b):
+    result = ""
+    for i in range(len(a)):
+        if a[i]==b[i]:
+            result += "0"
+        else:
+            result += "1"
+    return result
